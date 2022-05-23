5004978b8e9b6d55c6d2de1943d5dfe882cffc90
Devansh Gaur <89275109+Devansh-gaur-1611@users.noreply.github.com>
2022-05-24 03:27:33 +0530

    Loader added

---
+++ frontend/face-recognition/src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import styles from './Loader.module.css';
+
+
+const Loader = () => {
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.ldsDefault}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default Loader;
