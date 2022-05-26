17f8adbaf159bf42cce8709cba539ea555c69b78
Devansh Gaur <89275109+Devansh-gaur-1611@users.noreply.github.com>
2022-05-26 17:07:50 +0530

    Navbar and sidebar done

--- frontend/face-recognition/src/components/navbar/DepartmentSidenavBar.jsx
+++ frontend/face-recognition/src/components/navbar/DepartmentSidenavBar.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import SideNavBar from "./SideNavBar";
-import {
-  IoChevronBackOutline,
-  IoArrowBackCircleOutline,
-} from "react-icons/io5";
-
+import { IoChevronBackOutline, IoArrowBackCircleOutline } from "react-icons/io5";
+import { useDispatch } from "react-redux";
+import { smallSidebarClose } from "../../features/Sidebar/SidebarSlice";
 import styles from "./SideNavBar.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { Link, useParams } from "react-router-dom";
 
 const DepartmentSidenavBar = () => {
-  const params = useParams()
-  console.log(params);
+  // Declaring variables
+  const params = useParams();
+  const dispatch = useDispatch();
+
   return (
     <SideNavBar>
       <div className={styles.deptTopSection}>
-        <Link to={`/department`} className={styles.deptBackLink}>
+        <Link
+          to={`/department`}
+          className={styles.deptBackLink}
+          onClick={() => {
+            dispatch(smallSidebarClose());
+          }}
+        >
           <div className={styles.icon1}>
             <IoChevronBackOutline />
           </div>
@@ -26,15 +32,17 @@ const DepartmentSidenavBar = () => {
         </Link>
       </div>
       <div className={styles.middleContainer}>
-        <div
-          className={styles.iconContainer}
-          style={{ backgroundColor: "#00ff04" }}
-        >
+        <div className={styles.iconContainer} style={{ backgroundColor: "#00ff04" }}>
           <div className={styles.abv}>CB</div>
         </div>
         <div className={styles.deptName}>Crime Branch</div>
       </div>
-      <Link to={`/${params.className}/addParticipants`}>
+      <Link
+        to={`/${params.className}/addParticipants`}
+        onClick={() => {
+          dispatch(smallSidebarClose());
+        }}
+      >
         <div className={styles.bottomContainer}>
           <div className={styles.addIcon}>
             <AiOutlinePlusCircle />
