afac51463e7ecc9ec2ff0c2e8d2cb1ec2800eb9f
Devansh Gaur <89275109+Devansh-gaur-1611@users.noreply.github.com>
2022-05-26 18:54:21 +0530

    landing page done

---
+++ frontend/face-recognition/src/components/landingPage/AttendanceModal.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import styles from "./AttendanceModal.module.css";
+import { RiCloseLine } from "react-icons/ri";
+import { useSnackbar } from "notistack";
+import axios from "axios";
+import Loader from "../Loader/Loader";
+
+const AttendanceModal = ({ setTeamModalOpen, imageSrc }) => {
+  // Declaring variables
+  const [teamName, setTeamName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
+
+  // Handling the request for marking attandance
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (teamName == "") {
+      enqueueSnackbar("Enter your team name", {
+        variant: "warning",
+      });
+      setLoading(false);
+      return;
+    }
+
+    // Call for backend API
+    Promise.resolve(
+      axios.post(process.env.REACT_APP_NODE_API_URL+"markAttendence", {
+        imageSrc: imageSrc,
+        teamName: teamName,
+      })
+    )
+      .then((res) => {
+        enqueueSnackbar("Congratulations!!, Your attendence is marked", {
+          variant: "success",
+        });
+        setLoading(false);
+        setTeamModalOpen(false);
+      })
+      .catch((error) => {
+        enqueueSnackbar(error.response.data.error, {
+          variant: "error",
+        });
+        setLoading(false);
+        setTeamModalOpen(false);
+      });
+  };
+  return (
+    <>
+      {loading && <Loader />}
+      <div className={styles.darkBG} onClick={() => setTeamModalOpen(false)} />
+      <div className={styles.centered}>
+        <div className={styles.modal}>
+          <div className={styles.modalHeader}>
+            <h5 className={styles.heading}>Enter your Team Name</h5>
+          </div>
+          <button className={styles.closeBtn} onClick={() => setTeamModalOpen(false)}>
+            <RiCloseLine style={{ marginBottom: "-3px" }} />
+          </button>
+          <form className={styles.form} onSubmit={submitHandler}>
+            <div className={styles.modalContent}>
+              <label className={styles.label}>Team/Department Name</label>
+              <input type="text" className={styles.input} required onChange={(e) => setTeamName(e.target.value)} />
+            </div>
+            <div className={styles.modalActions}>
+              <input className={styles.submitBtn} type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AttendanceModal;
